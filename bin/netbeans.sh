5e8e75f2b898ba592378d9f9070e4267793aed8b
John Neffenger <john@status6.com>
2021-10-16 11:16:12 -0700

    Initial commit

---
+++ bin/netbeans.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# netbeans.sh - starts the Apache NetBeans IDE
+# Copyright 2021 John Neffenger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Location of the JDK in the OpenJDK Snap package
+jdkhome=$SNAP/../../openjdk/current/jdk
+
+# Uses the OpenJDK Snap, if installed, when JAVA_HOME is not set
+if [ ! -v JAVA_HOME ] && [ -d "$jdkhome" ]; then
+    JAVA_HOME=$jdkhome
+fi
+
+# Starts the NetBeans IDE
+if [ -v JAVA_HOME ]; then
+    "$SNAP/netbeans/bin/netbeans" --jdkhome "$JAVA_HOME"
+else
+    "$SNAP/netbeans/bin/netbeans"
+fi
